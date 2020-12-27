588a8b2a6df1bb89a4da714679db35f9d6cfead7
KhawlahAlorini <khawlasal7@gmail.com>
2020-12-27 09:17:56 +0300

    Add second page

--- js/secondPage.js
+++ js/secondPage.js
@@ -6,4 +6,19 @@ $(document).ready(function () {
     $(window).on("unload", function () {
         localStorage.clear();
     })
+
+
+    $("div").hover(
+        function () {
+            $(this).css("filter", "none")
+        },
+        function () {
+            $(this).css("filter", "blur(3px)")
+        })
+
+    $(".comparison").click(function(){
+        window.location.replace("/html/comparisonPage.html")
+    })
+
+    
 });
